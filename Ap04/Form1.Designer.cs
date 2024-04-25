11c845edec0d4fc2c508c91eabad6bdf3ad6ff7d
Jerry Huang <jerry@conic-soft.com>
2024-04-25 10:43:25 +0800

    Notepad V3.0

---
+++ Ap04/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Ap04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNewFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSaveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuView = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuForeColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuBackColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.dlgColor = new System.Windows.Forms.ColorDialog();
+            this.dlgFont = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtData
+            // 
+            this.txtData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtData.Location = new System.Drawing.Point(0, 24);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtData.Size = new System.Drawing.Size(813, 512);
+            this.txtData.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuView});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(813, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNewFile,
+            this.mnuOpenFile,
+            this.mnuSaveFile,
+            this.mnuQuit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(57, 20);
+            this.mnuFile.Text = "檔案(&F)";
+            // 
+            // mnuNewFile
+            // 
+            this.mnuNewFile.Name = "mnuNewFile";
+            this.mnuNewFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mnuNewFile.Size = new System.Drawing.Size(180, 22);
+            this.mnuNewFile.Text = "開新檔案";
+            this.mnuNewFile.Click += new System.EventHandler(this.mnuNewFile_Click);
+            // 
+            // mnuOpenFile
+            // 
+            this.mnuOpenFile.Name = "mnuOpenFile";
+            this.mnuOpenFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.mnuOpenFile.Size = new System.Drawing.Size(180, 22);
+            this.mnuOpenFile.Text = "開啟舊檔...";
+            this.mnuOpenFile.Click += new System.EventHandler(this.mnuOpenFile_Click);
+            // 
+            // mnuSaveFile
+            // 
+            this.mnuSaveFile.Name = "mnuSaveFile";
+            this.mnuSaveFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mnuSaveFile.Size = new System.Drawing.Size(180, 22);
+            this.mnuSaveFile.Text = "儲存檔案...";
+            this.mnuSaveFile.Click += new System.EventHandler(this.mnuSaveFile_Click);
+            // 
+            // mnuQuit
+            // 
+            this.mnuQuit.Name = "mnuQuit";
+            this.mnuQuit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.mnuQuit.Size = new System.Drawing.Size(180, 22);
+            this.mnuQuit.Text = "離開";
+            this.mnuQuit.Click += new System.EventHandler(this.mnuQuit_Click);
+            // 
+            // mnuView
+            // 
+            this.mnuView.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuForeColor,
+            this.mnuBackColor,
+            this.mnuFont});
+            this.mnuView.Name = "mnuView";
+            this.mnuView.Size = new System.Drawing.Size(59, 20);
+            this.mnuView.Text = "檢視(&V)";
+            // 
+            // mnuForeColor
+            // 
+            this.mnuForeColor.Name = "mnuForeColor";
+            this.mnuForeColor.Size = new System.Drawing.Size(180, 22);
+            this.mnuForeColor.Text = "文字顏色";
+            this.mnuForeColor.Click += new System.EventHandler(this.mnuForeColor_Click);
+            // 
+            // mnuBackColor
+            // 
+            this.mnuBackColor.Name = "mnuBackColor";
+            this.mnuBackColor.Size = new System.Drawing.Size(180, 22);
+            this.mnuBackColor.Text = "背景顏色";
+            this.mnuBackColor.Click += new System.EventHandler(this.mnuBackColor_Click);
+            // 
+            // mnuFont
+            // 
+            this.mnuFont.Name = "mnuFont";
+            this.mnuFont.Size = new System.Drawing.Size(180, 22);
+            this.mnuFont.Text = "字體";
+            this.mnuFont.Click += new System.EventHandler(this.mnuFont_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.Filter = "All files|*.*|Text Files|*.txt";
+            this.dlgOpen.FilterIndex = 2;
+            // 
+            // dlgSave
+            // 
+            this.dlgSave.Filter = "All files|*.*|Text Files|*.txt";
+            this.dlgSave.FilterIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 536);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "記事本";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuNewFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpenFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuSaveFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuQuit;
+        private System.Windows.Forms.ToolStripMenuItem mnuView;
+        private System.Windows.Forms.ToolStripMenuItem mnuForeColor;
+        private System.Windows.Forms.ToolStripMenuItem mnuBackColor;
+        private System.Windows.Forms.ToolStripMenuItem mnuFont;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+        private System.Windows.Forms.ColorDialog dlgColor;
+        private System.Windows.Forms.FontDialog dlgFont;
+    }
+}
+
---
+++ Ap04/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ap04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuNewFile_Click(object sender, EventArgs e)
+        {
+            txtData.Clear();
+        }
+
+        private void mnuOpenFile_Click(object sender, EventArgs e)
+        {
+            if(dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(dlgOpen.FileName);
+                txtData.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        private void mnuSaveFile_Click(object sender, EventArgs e)
+        {
+            if(dlgSave.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(dlgSave.FileName);
+                sw.Write(txtData.Text);
+                sw.Close();
+                MessageBox.Show("存檔完畢", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void mnuQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mnuForeColor_Click(object sender, EventArgs e)
+        {
+            if(dlgColor.ShowDialog()==DialogResult.OK)
+            {
+                txtData.ForeColor = dlgColor.Color;
+            }
+        }
+
+        private void mnuBackColor_Click(object sender, EventArgs e)
+        {
+            if (dlgColor.ShowDialog() == DialogResult.OK)
+            {
+                txtData.BackColor = dlgColor.Color;
+            }
+        }
+
+        private void mnuFont_Click(object sender, EventArgs e)
+        {
+            if(dlgFont.ShowDialog() == DialogResult.OK)
+            {
+                txtData.Font = dlgFont.Font;
+            }
+        }
+    }
+}
